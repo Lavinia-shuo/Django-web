1da86a9be5621f5e3b868fbfdd99b76b43f4dc01
Lavinia414 <chenshuo_414@163.com>
2021-11-15 20:05:24 +0800

    new

--- apgame/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from apgame.views import index
-
-urlpatterns = [
-        path("", index, name = "index"),
-]
--- apgame/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(resquest):
-        return HttpResponse("我的第一个网页！！！！")
