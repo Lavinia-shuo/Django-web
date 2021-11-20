9360d32f9c69242b57dac287644653ce5f99edd0
Lavinia414 <chenshuo_414@163.com>
2021-11-20 20:52:43 +0800

    3

--- apgame/static/js/src/playground/zbase.js
+++ apgame/static/js/src/playground/zbase.js
@@ -3,7 +3,7 @@ class AcGamePlayground {
         this.root = root;
         this.$playground = $(`<div class = "ac-game-playground" > < / div>`);
 
-        this.hide();
+       // this.hide();
         this.root.$ac_game.append(this.$playground);
         this.width = this.$playground.width();
         this.height = this.$playground.height();
